55b5600e335320b737a73ce98b6efff8fc26cfe7
Ger678 <95255946+Ger678@users.noreply.github.com>
2022-08-01 16:40:21 -0300

    nuevos componentes

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { BannerComponent } from './Components/banner/banner.component';
+import { QuoteComponent } from './Components/quote/quote.component';
+import { AboutComponent } from './Components/about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    QuoteComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
