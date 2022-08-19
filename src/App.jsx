e8cd008529d4649bdf4a815adb56bc21d9e567b2
Ger678 <95255946+Ger678@users.noreply.github.com>
2022-08-19 13:34:19 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import ButtonAppBar from './components/NavBar';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonAppBar />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,12 +3,16 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { ThemeProvider } from '@mui/material';
+import styles from '../pallete/pallete';
+
+const theme = styles;
 
 export default function ButtonAppBar() {
   return (
+    <ThemeProvider theme={theme}>
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
@@ -27,5 +31,6 @@ export default function ButtonAppBar() {
         </Toolbar>
       </AppBar>
     </Box>
+    </ThemeProvider>
   );
 }
